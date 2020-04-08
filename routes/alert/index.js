1cb9d352c17d076885b25381e14745a5760b43c9
Jatin Anand <31433847+Jatin345Anand@users.noreply.github.com>
2020-04-08 22:54:54 +0530

    updated

--- routes/alert/index.js
+++ routes/alert/index.js
@@ -4,6 +4,201 @@ const passport = require('passport');
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const myConfig = require('dotenv').config();
+var multer = require('multer')
+// define multer storage configuration     
+const storage = require('../../logics/file');
+const upload = multer({ storage: storage });
+const datetime = require('node-datetime');
+const dt = datetime.create();
+const formatted = dt.format('Y-m-d H:M:S');
+// pool
+router.post('/publish', passport.authenticate('jwt', { session: false }), upload.array('documents', 50), async (req, res, next) => {
+    console.log('in alert/publish');
+
+    // console.log('req,bosy is ', req.headers);
+    if (!req.body) {
+        res.json({ msg: "Please fill out complete form" });
+    }
+    if (!req.headers) {
+        res.json({ msg: "Please fill out token in headers" });
+    }
+    const token = req.headers.authorization;
+    console.log(process.env.IMS + '/user/profile');
+
+    var header = {
+        headers: {
+            Authorization: token
+        }
+    };
+    let info = await axios.get(process.env.IMS + '/user/profile', header);
+    const userIms = info.data;
+    console.log('is authorized :yes', userIms);
+    if (!userIms) {
+        return res.json({ 'msg': "unauthoried!! Please check token." });
+    }
+    
+    // Get Docs IDs from DMS of all Documents
+    if(!req.files){
+        return res.json({ 'msg': "Please upload documents" });
+    }
+    const files = req.files;
+    console.log(files);
+    var dmsFileOBj ={};
+    dmsFileOBj.files = files;
+    console.log(dmsFileOBj);
+    let info3 = await axios.post(process.env.DMS + '/api/document/upload/getId', dmsFileOBj);
+    var DocumentsIDs = info3.data
+    console.log('Documents ids from DMS ', DocumentsIDs);
+
+    if (!req.body.industry) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out industry field.',
+            },
+        });
+    }
+    if (!req.body.territory) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out territory field.',
+            },
+        });
+    }
+    if (!req.body.authority) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out authority field.',
+            },
+        });
+    }
+
+    if (!req.body.RegulationId) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out RegulationId field.',
+            },
+        });
+    }
+    if (!req.body.regulationDocType) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out regulationDocType field.',
+            },
+        });
+    }
+    if (!req.body.dateOfDeadline) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out dateOfDeadline field.',
+            },
+        });
+    }
+    if (!req.body.dateOfPublication) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out dateOfPublication field.',
+            },
+        });
+    }
+
+    if (!req.body.keywords) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out keywords field.',
+            },
+        });
+    }
+    if (!req.body.summary) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out Summary field.',
+            },
+        });
+    }
+    if (!req.body.clients) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out clients field.',
+            },
+        });
+    }
+    if (!req.body.source) {
+        return res.status(200).json({
+            errors: {
+                message: 'Please fill out source field.',
+            },
+        });
+    }
+    
+    var DocumentsIDs =[ 17 ]; // Recieved from DMS
+    const industry = req.body.industry;
+    const territory = req.body.territory;
+    var RegulationId = req.body.RegulationId;
+    const regulationDocType = req.body.regulationDocType;
+    const dateOfDeadline = req.body.dateOfDeadline;
+    const dateOfPublication = req.body.dateOfPublication;
+    const keywords = req.body.keywords;
+    const summary = req.body.summary;
+    const clients = req.body.clients;
+    const source = req.body.source;
+    var documentId = DocumentsIDs;
+    const authority = req.body.authority;
+    // Check Regulation Title Exist or Not
+    let info1 = await axios.get(process.env.RMS + '/api/reg/fetch/' + RegulationId);
+    var checkRegID = info1.data
+    if (checkRegID.length == 0) {
+        console.log('create new regulation ', checkRegID);
+        var regObj = {
+            "title": RegulationId,
+            "status": regulationDocType
+        }
+        let info2 = await axios.post(process.env.RMS + '/api/reg/create/getId', regObj);
+        RegulationId = info2.data;
+        console.log('new id ', RegulationId);
+    }
+    // Insert Regulation and Documents data in RegulationDoc Table (RMS)
+    var regDocBodyObj = {};
+    regDocBodyObj.reg_id = RegulationId;
+    regDocBodyObj.dms_doc_id=documentId;    
+
+    let info5 = await axios.post(process.env.RMS + '/api/regdoc/create/getId',regDocBodyObj);
+    const autoIdRegDoc = info5.data;
+    console.log('Stored in RegDoc Table',autoIdRegDoc);
+    // Store Data into AMS (Alert and Authority )
+    var alertCreateObj ={};
+    industry,territory,RegulationId,regulationDocType,dateOfDeadline,dateOfPublication,keywords,summary,clients,source,documentId
+    alertCreateObj.industry = industry;
+    alertCreateObj.territory = territory;
+    alertCreateObj.RegulationId = RegulationId;
+    alertCreateObj.regulationDocType = regulationDocType;
+    alertCreateObj.dateOfDeadline = dateOfDeadline;
+    alertCreateObj.dateOfPublication = dateOfPublication;
+    alertCreateObj.keywords = keywords;
+    alertCreateObj.summary = summary;
+    alertCreateObj.clients = clients;
+    alertCreateObj.source = source;
+    alertCreateObj.documentId = documentId;
+    alertCreateObj.authority = authority;
+    let info4 = await axios.post(process.env.AMS + '/alert/create',alertCreateObj);
+    const alertCreated = info4.data;
+    console.log('AMS alert created ',alertCreated);
+    let resposnseObj = {};
+    resposnseObj.message ='Alert Publish Successfully!!'
+      resposnseObj.AMS = alertCreated;
+
+    res.json(resposnseObj);
+}); 
+
+module.exports = router; 
+
+
+// old
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const myConfig = require('dotenv').config();
 
 router.post('/publish', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
     console.log('in alert/publish', myConfig.DMS);
